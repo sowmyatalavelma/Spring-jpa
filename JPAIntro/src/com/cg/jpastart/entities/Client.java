35b5201493c54ff313239f96f6bfc0c8a6c837be
sowmyatalavelma <sowmyatalavelma@gmail.com>
2021-04-28 13:43:32 +0530

    first commit

---
+++ JPAIntro/src/com/cg/jpastart/entities/Client.java
@@ -0,0 +1,27 @@
+package com.cg.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory factory= Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em= factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		Greet greet=new Greet();
+		greet.setMessage("Welcome to JPA!");
+		
+		em.persist(greet);
+		System.out.println("Added greeting to database.");
+		
+		em.getTransaction().commit();
+		em.close();
+		factory.close();
+
+	}
+
+}
